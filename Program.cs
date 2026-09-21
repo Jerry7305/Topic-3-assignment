20c959c3b588de636b61dd6be665eb476e4b7d6e
Jeremiah Mol <363325788@GoTVDSB.ca>
2026-09-21 13:18:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Topic_3_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jeremiah
+            int roomNumber;
+            double price;
+            string courseName;
+            string fullName;
+            int gradYear;
+           
+
+            roomNumber = 29;
+            price = 2.99;
+            courseName = "Computer Science";
+            fullName = "Jeremiah Mol";
+            gradYear = 2028;
+
+
+            Console.WriteLine("This is room #" + roomNumber);
+            Console.WriteLine("The price is $" + price);
+            Console.WriteLine("I am learning a bit about " + courseName);
+            Console.WriteLine("");
+            Console.WriteLine("My name is " + fullName + " and I'll graduate in " + gradYear);
+
+
+        }
+    }
+}
