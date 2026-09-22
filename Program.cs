9a57f06d3888df671866b6545d20b7d64d6a4a26
Jeremiah Mol <363325788@GoTVDSB.ca>
2026-09-22 13:16:37 -0400

    Almost done

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@
             string courseName;
             string fullName;
             int gradYear;
+            double radius;
+            double area;
+            double a2;
+            double b2;
+            double c2;
+
            
 
             roomNumber = 29;
@@ -17,13 +23,25 @@
             courseName = "Computer Science";
             fullName = "Jeremiah Mol";
             gradYear = 2028;
+            radius = 15;
+            a2 = Math.Pow(7,2);
+            b2 = Math.Pow(5, 2);
+            c2 = a2 + b2;
+
+            area = double.Pi * (radius * radius); // Area of a cirle formula
 
 
             Console.WriteLine("This is room #" + roomNumber);
             Console.WriteLine("The price is $" + price);
             Console.WriteLine("I am learning a bit about " + courseName);
             Console.WriteLine("");
-            Console.WriteLine("My name is " + fullName + " and I'll graduate in " + gradYear);
+            Console.WriteLine("My name is " + fullName + " and I'll graduate in " + gradYear + ".");
+            Console.WriteLine("");
+            Console.WriteLine(Math.Round(area,2)); // Area of a circle with a radius of 15 rounded to 2 decimals
+            Console.WriteLine("");
+            Console.WriteLine("My favourite formula is pythagoream theorem");
+            Console.WriteLine(
+
 
 
         }
